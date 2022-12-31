6af33cb7e2bd2444bd5419b507ca585dc95c29a5
hyggge <hyggge6@gmail.com>
2022-12-31 23:31:22 +0800

    创建article的model和controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_31_152632) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_31_152943) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "comments", force: :cascade do |t|
     t.integer "grade"
     t.text "content"
