7f923ad44365ec2eda0707fca3e5a22240fd1b6b
hyggge <hyggge6@gmail.com>
2022-12-31 23:15:55 +0800

    创建comment的model和controller

---
+++ app/models/comment.rb
@@ -0,0 +1,4 @@
+class Comment < ApplicationRecord
+  validates :grade, presence: true, numericality: { in: 1..5 }
+  validates :content, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_31_150441) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_31_151018) do
+  create_table "comments", force: :cascade do |t|
+    t.integer "grade"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "commodities", force: :cascade do |t|
     t.string "name"
     t.text "introduction"
