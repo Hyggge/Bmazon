b990d22f96ae4bdb54779ced9e7ef5297ae54b11
hyggge <hyggge6@gmail.com>
2022-12-31 23:35:14 +0800

    创建image的model和controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_31_153227) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_31_153401) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -33,6 +33,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_31_153227) do
     t.integer "sale"
     t.decimal "price", precision: 10, scale: 2
     t.integer "method"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
