4e10dc2eb622557230af4c65c0b1371b5b51eb04
hyggge <hyggge6@gmail.com>
2023-01-03 16:25:23 +0800

    调整获取用户详情的返回信息

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,22 +5,29 @@ class UsersController < ApplicationController
   # [GET] /api/users/<int:user_id>
   def show_details
     @user = User.find_by(id: params[:user_id])
-    if @user
-      res = {
-        username: @user.username,
-        reg_time: @user.created_at,
-        phone_no: @user.phone_no,
-        email: @user.email,
-        signature: @user.signature,
-        is_admin: @user.role,
-        status: @user.student_id == nil ? 0 : 1,
-        student_info: @user.student_id == nil ? nil : @user.student,
-        image_url: @user.image == nil ? nil : @user.image.url
-      }
-      render json: res, status: :ok
-    else
-      render json: {error: 'user_id cannot be found'}, status: :not_found
-    end
+
+    student_info = {
+      id: @user.student.id,
+      name: @user.student.name,
+      depart: @user.student.depart,
+      attendance_year: @user.student.attendance_year,
+    } if @user.student
+
+    res = {
+      id: @user.id,
+      username: @user.username,
+      reg_time: @user.created_at,
+      phone_no: @user.phone_no,
+      email: @user.email,
+      signature: @user.signature,
+      role: @user.role,
+      status: @user.student_id == nil ? 0 : 1,
+      student_info: @user.student_id == nil ? nil : student_info,
+      image_url: @user.image == nil ? nil : @user.image.url
+    }
+
+    render json: res, status: :ok
+
 
   end
 
