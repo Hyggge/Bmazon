6aca46bc086d1cd04a51e76a1d67c16335dbc37d
hyggge <hyggge6@gmail.com>
2023-01-05 17:02:43 +0800

    美化首页

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,11 @@
 <template>
   <d2-container>
     <template v-slot:header>
-      <h1 style="display: flex; justify-content: center;">欢迎来到北航交易中心</h1>
+      <h1 style="display: flex; justify-content: center;">欢迎来到北航校内交易中心</h1>
     </template>
-
-    <img class="left-pic" src="../../assets/img/web_shopping.svg" alt=""/>
+    <div style="text-align: center">
+      <img class="left-pic" src="../../assets/img/web_shopping.svg" alt="" style="width: 50%"/>
+    </div>
   </d2-container>
 </template>
 
